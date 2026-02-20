dc5cb8ab6e558603e607c3389500f2c95e7cb4b1
bekirkucukoglu <bekirbekobek@gmail.com>
2026-02-20 17:34:38 +0000

    Update app.py

--- CoordiNote API/app.py
+++ CoordiNote API/app.py
@@ -15,7 +15,7 @@ from utils import format_geojson
 # Database configuration
 
 DB_CONFIG = {
-    "database": "coordinote_db", # The name of the database we will connect to (specific for me, Marie - we need to update this)
+    "database": "coordinote_share", # The name of the database we will connect to (specific for me, Marie - we need to update this)
     "user": "postgres",
     "password": "postgres",
     "host": "localhost",
